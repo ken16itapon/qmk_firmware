2c9ed3abf53878c7258c769f4fe18db63310a356
ken16itapon <46488674+ken16itapon@users.noreply.github.com>
2025-02-17 14:04:43 +0900

    14

--- keyboards/helix/rev3_5rows/config.h
+++ keyboards/helix/rev3_5rows/config.h
@@ -98,4 +98,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define DEBUG_MATRIX_SCAN_RATE
 #    define DEBUG_MATRIX
 #endif
-
--- keyboards/helix/rev3_5rows/mcuconf.h
+++ keyboards/helix/rev3_5rows/mcuconf.h
@@ -18,6 +18,3 @@
 #pragma once
 
 #include_next <mcuconf.h>
-
-#undef RP_SPI_USE_SPI0
-#define RP_SPI_USE_SPI0 TRUE
